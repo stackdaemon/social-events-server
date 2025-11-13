60a5be21ae37bb3fc568e9f5128c3c40704f5a37
stackdaemon <mdrifathasan5127@gmail.com>
2025-11-13 16:22:31 +0600

    Add GET /events endpoint to fetch upcoming events only

--- index.js
+++ index.js
@@ -1,6 +1,69 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
 const app = express();
 const port = 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://social-events:m0QU0r2Rc60lsYDy@cluster0.zaaoh0g.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("social-events");
+    const eventCollection = db.collection("events");
+    const joinedEventsCollection = db.collection("joined-event");
+    // get data [1]
+    app.get("/events", async (req, res) => {
+      const result = await eventCollection.find().toArray();
+      res.send(result);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
